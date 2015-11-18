b7c73c45b73c5f3a1ab001057922c8a3c57a5cf9
Adrian S. Leven <adrianleven1000@gmail.com>
2015-11-18 01:20:47 -0800

    first commit
    farts

---
+++ app/controllers/feedback_controller.rb
@@ -0,0 +1,19 @@
+class FeedbackController < ApplicationController
+  def home
+  end
+
+  def food_selection
+  end
+
+  def quantity
+  end
+
+  def descriptors
+  end
+
+  def vote
+  end
+
+  def end
+  end
+end
