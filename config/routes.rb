c06eaa04ebe13161146f0b29adc132f58e1b6659
Andrei Terentiev <terentiev.andrei@gmail.com>
2015-11-30 19:20:40 -0800

    working on calendaqrr

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   get 'feedback/vote'
 
   get 'feedback/end'
+
+  get 'feedback/chef_calendar'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
